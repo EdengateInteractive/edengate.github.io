4400cc2637b38f500a7ef612c141b2f63ab0539e
EdengateInteractive <161099777+EdengateInteractive@users.noreply.github.com>
2024-05-01 00:10:57 -0400

    Update newspost.js

--- js/newspost.js
+++ js/newspost.js
@@ -152,17 +152,6 @@ function handleClick(event) {
     }
 }
 
-function handleAccessDeniedProjectCrimson() {
-    const gameSection = document.querySelector('.game-section.left-layout .game-info');
-    gameSection.innerHTML = '<h2>ACCESS DENIED</h2>';
-    setTimeout(function () {
-        gameSection.innerHTML = `
-            <h2>Project Crimson</h2>
-            <p>[REDACTED]</p>
-        `;
-    }, 2000); // Change the delay time as needed
-}
-
 window.onload = function() {
     fetchAndDisplayPosts();
 };
